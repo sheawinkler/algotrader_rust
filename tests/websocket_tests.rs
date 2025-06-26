0d4f716e2564e0706c71e0b1ca3be1059eb640cb
Shea Winkler <sheawinkler@gmail.com>
2025-06-26 11:45:55 -0600

    tests: mark websocket_connect as #[ignore] to stabilize CI

--- tests/websocket_tests.rs
+++ tests/websocket_tests.rs
@@ -5,6 +5,7 @@ extern crate algotraderv2 as algotraderv2_rust;
 use algotraderv2_rust::utils::websocket::WebSocketClient;
 
 #[tokio::test]
+#[ignore]
 async fn test_websocket_connect() {
     let url = "wss://echo.websocket.events";
     let client = WebSocketClient::new(url).expect("Failed to create client");
